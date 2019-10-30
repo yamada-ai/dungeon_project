953a315c29a07260d8ff13be34b4a73fef8f7f3c
tadaren <ptymaato@yahoo.co.jp>
2019-10-30 16:53:04 +0900

    通路をクラスとして管理するように変更

--- Dungeon.py
+++ Dungeon.py
@@ -2,6 +2,8 @@ from typing import List
 
 import numpy as np
 import random
+
+from Road import Road
 from Room import Room, RoomInfo
 
 
@@ -37,6 +39,8 @@ class Dungeon:
         self.rooms: List[Room] = []
         # 部屋を作る際に必要な区画の座標を保持する変数
         self.room_info: List[RoomInfo] = []
+        # 通路
+        self.roads: List[Road] = []
 
     # フロアマップを分割する
     def _div_floor(self, row_s, column_s, row_e, column_e):
@@ -111,74 +115,15 @@ class Dungeon:
                     break
 
     def _create_road(self, room1_info, room1, room2_info, room2):
-        # 上下に接続している
-        if room1_info.top == room2_info.bottom + 1 or room1_info.bottom + 1 == room2_info.top:
-            x1 = random.randint(room1.origin[1], room1.origin[1] + room1.size[1] - 1)
-            x2 = random.randint(room2.origin[1], room2.origin[1] + room2.size[1] - 1)
-            # room1が上側
-            if room1_info.top < room2_info.top:
-                y1 = room1.origin[0] + room1.size[0]
-                y2 = room2.origin[0]
-                # 縦方向に通路を引く
-                # room1
-                for j in range(y1, room1_info.bottom):
-                    self.floor_map[j][x1] = 3
-                # room2
-                for j in range(y2 - 1, room2_info.top - 1, -1):
-                    self.floor_map[j][x2] = 3
-                # 縦方向の通路を結ぶ
-                for j in range(min(x1, x2), max(x1, x2) + 1):
-                    self.floor_map[room1_info.bottom][j] = 3
-            # room2が上側
-            else:
-                y1 = room1.origin[0]
-                y2 = room2.origin[0] + room2.size[0]
-                # 横方向に通路を引く
-                # room1
-                for j in range(y1 - 1, room1_info.top - 1, -1):
-                    self.floor_map[j][x1] = 3
-                # room2
-                for j in range(y2, room2_info.bottom):
-                    self.floor_map[j][x2] = 3
-                # 縦方向の通路を結ぶ
-                for j in range(min(x1, x2), max(x1, x2) + 1):
-                    self.floor_map[room2_info.bottom][j] = 3
-            return True
+        road = Road(room1, room1_info, room2, room2_info)
+        if not road.can_connect:
+            return False
+        self.roads.append(road)
+        return True
 
-        # 左右に接続している
-        if room1_info.left == room2_info.right + 1 or room1_info.right + 1 == room2_info.left:
-            y1 = random.randint(room1.origin[0], room1.origin[0] + room1.size[0] - 1)
-            y2 = random.randint(room2.origin[0], room2.origin[0] + room2.size[0] - 1)
-            # room1が左側
-            if room1_info.left < room2_info.left:
-                x1 = room1.origin[1] + room1.size[1]
-                x2 = room2.origin[1]
-                # 横方向に通路を引く
-                # room1
-                for j in range(x1, room1_info.right):
-                    self.floor_map[y1][j] = 3
-                # room2
-                for j in range(x2 - 1, room2_info.left - 1, -1):
-                    self.floor_map[y2][j] = 3
-                # 横方向の通路を結ぶ
-                for j in range(min(y1, y2), max(y1, y2) + 1):
-                    self.floor_map[j][room1_info.right] = 3
-            # room2が左側
-            else:
-                x1 = room1.origin[1]
-                x2 = room2.origin[1] + room2.size[1]
-                # 横方向に通路を引く
-                # room1
-                for j in range(x1 - 1, room1_info.left - 1, -1):
-                    self.floor_map[y1][j] = 3
-                # room2
-                for j in range(x2, room2_info.right):
-                    self.floor_map[y2][j] = 3
-                # 横方向の通路を結ぶ
-                for j in range(min(y1, y2), max(y1, y2) + 1):
-                    self.floor_map[j][room2_info.right] = 3
-            return True
-        return False
+    def _print_roads2map(self):
+        for road in self.roads:
+            road.print2map(self.floor_map)
 
     def print_floor_map(self):
         color_format = (
@@ -215,5 +160,6 @@ if __name__ == "__main__":
     dungeon._print_rooms2map()
     # dungeon.scaling()
     dungeon._connect_rooms()
+    dungeon._print_roads2map()
     dungeon.floor_map[dungeon.floor_map == 1] = 0
     dungeon.print_floor_map()
---
+++ Road.py
@@ -0,0 +1,100 @@
+import random
+from typing import Tuple, List
+import numpy as np
+
+from Room import Room, RoomInfo
+
+
+class Road:
+    def __init__(self, room1: Room, room1_info: RoomInfo, room2: Room, room2_info: RoomInfo):
+        self.room1 = room1
+        self.room1_info = room1_info
+        self.room2 = room2
+        self.room2_info = room2_info
+        self.cells: List[Tuple[int, int]] = []
+
+        self.can_connect = self._can_connect()
+        if self.can_connect:
+            self._connect()
+
+    def _can_connect(self):
+        if self.room1_info.top == self.room2_info.bottom + 1 or self.room1_info.bottom + 1 == self.room2_info.top:
+            return True
+        if self.room1_info.left == self.room2_info.right + 1 or self.room1_info.right + 1 == self.room2_info.left:
+            return True
+        return False
+
+    def _connect(self):
+        room1 = self.room1
+        room2 = self.room2
+        room1_info = self.room1_info
+        room2_info = self.room2_info
+        # 上下に接続している
+        if room1_info.top == room2_info.bottom + 1 or room1_info.bottom + 1 == room2_info.top:
+            x1 = random.randint(room1.origin[1], room1.origin[1] + room1.size[1] - 1)
+            x2 = random.randint(room2.origin[1], room2.origin[1] + room2.size[1] - 1)
+            # room1が上側
+            if room1_info.top < room2_info.top:
+                y1 = room1.origin[0] + room1.size[0]
+                y2 = room2.origin[0]
+                # 縦方向に通路を引く
+                # room1
+                for j in range(y1, room1_info.bottom):
+                    self.cells.append((j, x1))
+                # room2
+                for j in range(y2 - 1, room2_info.top - 1, -1):
+                    self.cells.append((j, x2))
+                # 縦方向の通路を結ぶ
+                for j in range(min(x1, x2), max(x1, x2) + 1):
+                    self.cells.append((room1_info.bottom, j))
+            # room2が上側
+            else:
+                y1 = room1.origin[0]
+                y2 = room2.origin[0] + room2.size[0]
+                # 横方向に通路を引く
+                # room1
+                for j in range(y1 - 1, room1_info.top - 1, -1):
+                    self.cells.append((j, x1))
+                # room2
+                for j in range(y2, room2_info.bottom):
+                    self.cells.append((j, x2))
+                # 縦方向の通路を結ぶ
+                for j in range(min(x1, x2), max(x1, x2) + 1):
+                    self.cells.append((room2_info.bottom, j))
+
+        # 左右に接続している
+        if room1_info.left == room2_info.right + 1 or room1_info.right + 1 == room2_info.left:
+            y1 = random.randint(room1.origin[0], room1.origin[0] + room1.size[0] - 1)
+            y2 = random.randint(room2.origin[0], room2.origin[0] + room2.size[0] - 1)
+            # room1が左側
+            if room1_info.left < room2_info.left:
+                x1 = room1.origin[1] + room1.size[1]
+                x2 = room2.origin[1]
+                # 横方向に通路を引く
+                # room1
+                for j in range(x1, room1_info.right):
+                    self.cells.append((y1, j))
+                # room2
+                for j in range(x2 - 1, room2_info.left - 1, -1):
+                    self.cells.append((y2, j))
+                # 横方向の通路を結ぶ
+                for j in range(min(y1, y2), max(y1, y2) + 1):
+                    self.cells.append((j, room1_info.right))
+            # room2が左側
+            else:
+                x1 = room1.origin[1]
+                x2 = room2.origin[1] + room2.size[1]
+                # 横方向に通路を引く
+                # room1
+                for j in range(x1 - 1, room1_info.left - 1, -1):
+                    self.cells.append((y1, j))
+                # room2
+                for j in range(x2, room2_info.right):
+                    self.cells.append((y2, j))
+                # 横方向の通路を結ぶ
+                for j in range(min(y1, y2), max(y1, y2) + 1):
+                    self.cells.append((j, room2_info.right))
+
+    def print2map(self, floor_map: np.ndarray):
+        for cell in self.cells:
+            floor_map[cell] = 3
