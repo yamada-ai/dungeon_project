d206b452d0653c437d474f8015f1a57ce4cefa35
tadaren <ptymaato@yahoo.co.jp>
2019-11-16 20:42:07 +0900

    デバッグ用のページを修正に対応するようにした

--- static/app.js
+++ static/app.js
@@ -1,11 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
+        mode: "2",
         raw: '',
         id: 'NO DATA',
         isEnd: false,
         map: [],
-        floor_map: [[]]
+        floor_map: [[]],
+        reward: 0,
     },
     created(){
         document.onkeydown = () => {
@@ -14,40 +16,51 @@ var app = new Vue({
     },
     methods: {
         createSimulator: async function () {
-            console.log('create');
+            let data = {
+                mode: parseInt(this.mode)
+            };
             await axios
-                .get('/create')
+                .post('/init', data)
                 .then(response => {
-                    this.raw = response.data;
+                    this.raw = response.data
                     this.id = response.data.id
                 });
+            this.refresh();
         },
         setData: function(data){
             this.raw = data;
             this.isEnd = data.isEnd;
-            this.floor_map = data.map;
+            this.floor_map = data.map.cells;
+            let room = data.map.rooms[data.roomId];
             if (this.isEnd) {
-                if (this.floor_map[data.agent.y][data.agent.x] === 5) {
+                if (this.floor_map[data.y + room.origin[0]][data.x + room.origin[1]] === 5) {
                     (async () => {
-                        await this.createSimulator();
+                        // await this.createSimulator();
+                        await axios.post("/reset/" + this.id)
                         this.refresh();
                     })();
                     return
                 }
-                this.floor_map[data.agent.y][data.agent.x] = 6;
+                this.floor_map[data.y + room.origin[0]][data.x + room.origin[1]] = 6;
             } else {
-                this.floor_map[data.agent.y][data.agent.x] = 3;
+                this.floor_map[data.y + room.origin[0]][data.x + room.origin[1]] = 3;
             }
             data.enemies.forEach(element => {
-                this.floor_map[element.y][element.x] = 4;
+                if(element.x !== -1 && element.y !== -1){
+                    this.floor_map[element.y][element.x] = 4;
+                }
             });
         },
         refresh: function(){
-            console.log('refresh');
             axios
             .get('/info/'+this.id)
             .then(response => {
-                this.setData(response.data);
+                if(this.mode == "1"){
+                    console.log(response.data);
+                    this.raw = response.data;
+                }else{
+                    this.setData(response.data);
+                }
             })
         },
         postAction: function(data){
