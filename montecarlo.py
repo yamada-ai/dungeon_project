b4707f50e3397fe9ed6674dbf9b332fe697153d7
Tadaren <ptymaato@yahoo.co.jp>
2019-11-20 20:39:00 +0900

    モンテカルロです

--- montecarlo.py
+++ montecarlo.py
@@ -1,12 +1,55 @@
+import time
+
 from simulator import *
 import random
 import numpy as np
 
 
-def learnCellMove():
+def select_action1(actions, eps):
+    a = random.random()
+    if a <= eps:
+        return random.randint(0, len(actions)-1)
+    return actions.argmax()
+
+
+def learn_room_move():
+    q = np.random.random((5, 5))
+    eps = [0.99]*5
+    sum_r = [[0.0]*5 for _ in range(5)]
+    sum_c = [[0]*5 for _ in range(5)]
+
+    log = []
     random.seed(0)
-    sim = CellMoveSimulator({})
-    sim
+    sim = RoomGraphSimulator()
+    random.seed()
+
+    for step in range(100):
+        state = sim.info()
+        is_end = False
+        sum_reward = 0
+        while not is_end:
+            action = select_action1(q[state['roomId']], eps[state['roomId']])
+            eps[state['roomId']] *= eps[state['roomId']]
+            log.append((
+                state['roomId'],
+                action
+            ))
+            reward = sim.action(int(action))
+            next_state = sim.info()
+            sum_reward += reward
+            if reward != 0:
+                for rule in log:
+                    sum_r[rule[0]][rule[1]] += sum_reward
+                    sum_c[rule[0]][rule[1]] += 1
+                    q[rule[0]][rule[1]] = sum_r[rule[0]][rule[1]] / sum_c[rule[0]][rule[1]]
+                    log.clear()
+                    sum_reward = 0
+            if reward == 100:
+                is_end = True
+            state = next_state
+        sim.reset()
+    return [int(e.argmax()) for e in q]
+
 
 def enemy(state):
     out = []
@@ -23,56 +66,97 @@ def enemy(state):
         out.append((2, 2))
     return out
 
+
 def select_action(q, eps):
-    r = random.random();
+    r = random.random()
     if r < eps:
         return random.randint(0, 4)
     return q.argmax()
 
-def main():
-    alpha = 0.1
-    gamma = 0.8
-    q = np.zeros((5, 13, 12, 5, 5, 5, 5, 5))
-    eps = np.full((5, 13, 12, 5, 5, 5, 5), 0.99)
-    # sum_r = np.zeros((5, 13, 12, 5, 5, 5, 5, 5))
-    # sum_c = np.zeros((5, 13, 12, 5, 5, 5, 5, 5), dtype=np.int64)
+
+def test(sim, q):
     random.seed(0)
-    sim = CellMoveSimulator({'firstRoom': 2})
-    
-    for _ in range(10000):
-        state = sim.info()
-        e = enemy(state)
-        sum_reward = 0
-        while not state['isEnd']:
-            action = select_action(q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]], eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]]);
-            eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]] *= eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]]
-
-            reward = sim.action(int(action))
-            next_state = sim.info()
-            e2 = enemy(next_state)
-
-            sum_reward += reward
-            q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1], action] = (1.0-alpha)*q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1], action] + alpha*(reward + gamma*q[next_state['roomId'], next_state['x'], next_state['y'], e2[0][0], e2[0][1], e2[1][0], e2[1][1]].max())
-            state = next_state
-            e = e2
-        sim.reset()
-        print(sum_reward)
-
-    random.seed(0)
-    sim = Simulator2({'firstRoom': 2})
     state = sim.info()
     e = enemy(state)
-    sum_reward = 0
     while not state['isEnd']:
-        action = select_action(q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]], eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]]);
+        action = select_action(q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]], 0)
 
-        reward = sim.action(int(action))
+        sim.action(int(action))
         next_state = sim.info()
         e2 = enemy(next_state)
 
         state = next_state
         e = e2
+    sim.reset()
+
+
+def main():
+    q1 = np.array(learn_room_move())
+    print(q1)
+    # alpha = 0.1
+    # gamma = 0.8
+    q = np.zeros((5, 13, 12, 5, 5, 5, 5, 5))
+    eps = np.full((5, 13, 12, 5, 5, 5, 5), 0.99)
+    sum_r = np.zeros((5, 13, 12, 5, 5, 5, 5, 5))
+    sum_c = np.zeros((5, 13, 12, 5, 5, 5, 5, 5), dtype=np.int64)
+    random.seed(0)
+    sim = CellMoveSimulator({})
+    random.seed(0)
+    t = Simulator2({})
+    random.seed()
+
+    log = []
+    for step in range(100000):
+        print(step, '/', 100000)
+        state = sim.info()
+        e = enemy(state)
+        sum_reward = 0
+        while not state['isEnd']:
+            print(state['roomId'], state['x'], state['y'], 'reward:', sum_reward, '\r', end='')
+            action = select_action(q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]], eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]])
+            eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]] *= eps[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1]]
+            log.append((
+                state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1],
+                action
+            ))
+
+            reward = sim.action({'action': int(action), 'nextRoomId': q1[state['roomId']]})
+            next_state = sim.info()
+            e2 = enemy(next_state)
+
+            sum_reward += reward
+            # q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1], action] = (1.0-alpha)*q[state['roomId'], state['x'], state['y'], e[0][0], e[0][1], e[1][0], e[1][1], action] + alpha*(reward + gamma*q[next_state['roomId'], next_state['x'], next_state['y'], e2[0][0], e2[0][1], e2[1][0], e2[1][1]].max())
+            if reward != 0:
+                for rule in log:
+                    sum_r[rule] += sum_reward
+                    sum_c[rule] += 1
+                    q[rule] = sum_r[rule] / sum_c[rule]
+                    log.clear()
+                    sum_reward = 0
+            state = next_state
+            e = e2
+        sim.reset()
+        if step % 10000:
+            test(t, q)
+
+    np.save('q_table1.npy', q1)
+    np.save('q_table2.npy', q)
+
+    for _ in range(5):
+        test(t, q)
+        time.sleep(1)
+
+
+def load():
+    q1 = np.load('q_table1.npy')
+    q = np.load('q_table2.npy')
+    random.seed(0)
+    sim = Simulator2({})
+    for _ in range(5):
+        test(sim, q)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     main()
+    # load()
